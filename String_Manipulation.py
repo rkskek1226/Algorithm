1268b75e4eed5e766e2e42611cc0b45dd499434a
rkskek1226 <hkcho01@naver.com>
2022-03-09 23:27:58 +0900

    2022/03/09

---
+++ String_Manipulation.py
@@ -0,0 +1,47 @@
+import collections
+import heapq
+import functools
+import itertools
+import re,sys, math, bisect
+from typing import *
+
+# 문자열 작업은 슬라이싱으로 처리하는 것이 바름
+# s1 = "12345"   s2 = s1[::-1] -> s1 문자열 뒤집은 것을 저장
+# s1 = ["1, 2, 3]   s2 = s1[::-1] -> s1 원소들을 뒤집은 것을 저장
+# 플랫폼마다 s1 = s2[::-1]이 되는 곳이 있고 안되는 곳이 있는데 안되는 곳에서는 s1[:] = s2[::-1]로 할 것
+
+# reverse()는 리스트의 요소들을 뒤집어 줌(리턴이 따로 없음)
+
+# 팰린드롬(Palindrome)
+# 1. 리스트로 푸는 방법
+def is_palindrome(self, s: str) -> bool:
+    strs = []
+
+    for char in s:
+        if char.isalnum():
+            strs.append(char.lower())
+
+    while len(strs) > 1:
+        if strs.pop(0) != strs.pop():
+            return False
+
+    return True
+
+
+# 2. 데크로 푸는 방법
+def is_palindrome(self, s: str) -> bool:
+    strs: Deque = collections.deque()
+
+    for char in s:
+        if char.isalnum():
+            strs.append(char.lower())
+
+    while len(strs) > 1:
+        if strs.popleft() != strs.pop():   # 리스트의 pop(0)은 O(n)이지만 데크의 popleft()는 O(1)
+            return False
+
+    return True
+
+
+
+
--- python_basic.py
+++ python_basic.py
@@ -1,3 +1,5 @@
+import collections
+
 # 타입 명시하는 방법
 a: str = "1"
 b: int = 1
@@ -51,6 +53,67 @@ def sample3():
 
 
 sample3()
+
+
+# 비교 연산자 is는 id() 값을 비교하고 ==는 값을 비교함
+
+# mutable object : list, set, dict
+# immutable object : bool, int, float, tuple, str,
+
+# 리스트에서 append()로 추가하거나 pop()으로 마지막 요소를 추출하거나 조회하는 것은 O(1)
+# 리스트에서 요소를 삭제하거나 첫번째 요소를 추출하는 pop(0)은 O(n)
+l1 = list()
+l2 = []
+l3 = [1, 2, 3]
+l3.append(4)
+l3.insert(3, 5)
+del l3[2]   # del은 인덱스 위치에 있는 요소를 삭제
+l3.pop(2)   # pop은 인덱스 위치에 있는 요소를 삭제
+l3.remove(2)   # remove는 해당 요소를 삭제
+try:
+    print(l3[5])
+except IndexError:
+    print("존재하지 않는 인덱스")
+
+
+# 딕셔너리(파이썬 3.7부터는 입력 순서가 유지됨)
+d1 = dict()
+d2 = {}
+d3 = {"k1": "v1", "k2": "v2"}
+d3["k3"] = "v3"
+print(d3)
+print(d3["k1"])
+try:
+    print(d3["k4"])
+except KeyError:
+    print("존재하지 않는 키")
+
+for k, v in d3.items():
+    print(k, v)
+
+del d3["k1"]   # 딕셔너리 키 삭제
+
+# defaultdict 객체 : 존재하지 않는 키를 조회할 경우 디폴트 값을 기준으로 해당 키에 대한 아이템을 생성해줌(에러 메세지 출력 X)
+d4 = collections.defaultdict(int)
+d4["A"] = 1
+d4["B"] = 2
+d4["C"] += 1
+
+# Counter 객체 : 아이템 갯수를 계산해 딕셔너리로 리턴
+tmp = [1, 1, 2, 2, 3, 3, 4, 5, 5, 1, 1]
+d5 = collections.Counter(tmp)
+print(d5.most_common(3))   # 빈도 수가 높은 요소를 추출
+
+# OrderedDict 객체 : 입력 순서가 유지되는 객체
+d6 = collections.OrderedDict({"A": 1, "B": 3, "C": 2})
+
+
+# isalnum() : 영어와 숫자로 구성되었는지 판별
+
+
+
+
+
 
 
 
