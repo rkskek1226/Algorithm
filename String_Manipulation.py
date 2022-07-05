4d8b5d10db940cc19e00a26bbcb8bd84bd105f33
rkskek1226 <hkcho01@naver.com>
2022-07-05 22:55:06 +0900

    2022/07/05

--- String_Manipulation.py
+++ String_Manipulation.py
@@ -10,6 +10,7 @@ from typing import *
 # s1 = ["1, 2, 3]   s2 = s1[::-1] -> s1 원소들을 뒤집은 것을 저장
 # 플랫폼마다 s1 = s2[::-1]이 되는 곳이 있고 안되는 곳이 있는데 안되는 곳에서는 s1[:] = s2[::-1]로 할 것
 
+
 # 팰린드롬(Palindrome)(뒤집어도 같은 말이 되는 단어나 문장)
 # 1. 리스트로 푸는 방법
 def is_palindrome(self, s: str) -> bool:
@@ -41,5 +42,30 @@ def is_palindrome(self, s: str) -> bool:
     return True
 
 
+
+# 문자열 뒤집기
+def reverseString(self, s: List[str]) -> None:
+    # 1. reverse() 사용
+    s.reverse()
+
+    # 2. 투 포인터(Two Pointer)로 스왑
+    left, right = 0, len(s) - 1
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
+
+
+# 로그파일 재정렬
+def reorderLogFiles(self, logs: List[str]) -> List[str]:
+    letters_log, digits_log = [], []
+    for log in logs:
+        if log.split(" ")[1].isdigit():
+            digits_log.append(log)
+        else:
+            letters_log.append(log)
+
+    letters_log.sort(key=lambda x: (x.split(" ")[1:], x.split(" ")[0]))
+    return letters_log + digits_log
 
 
