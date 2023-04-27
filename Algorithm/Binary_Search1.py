3f3affb1b1bef9484b24c95df31074ac9ae32516
rkskek1226 <hkcho01@naver.com>
2023-04-27 23:39:37 +0900

    Binary_Search

--- Algorithm/Binary_Search1.py
+++ Algorithm/Binary_Search1.py
@@ -1,4 +1,6 @@
 import bisect
+import sys
+
 
 # bisect 라이브러리는 정렬된 리스트에서 특정 원소를 찾을 때 효과적
 # bisect_left(a, x) : 정렬된 순서를 유지하면서 리스트 a에 데이터 x를 삽입할 가장 왼쪽 인덱스를 리턴
@@ -15,9 +17,7 @@ print(bisect.bisect_left(arr, 4))   # 2 리턴
 print(bisect.bisect_right(arr, 4))   # 4 리턴
 
 
-
 # bisect_left()와 bisect_right()를 활용하면 정렬된 리스트에서 값이 특정 범위에 속하는 원소의 개수를 효과적으로 구할 수 있음
-
 def count_by_range(a, left_value, right_value):
     right_idx = bisect.bisect_right(a, right_value)
     left_idx = bisect.bisect_left(a, left_value)
@@ -27,3 +27,64 @@ def count_by_range(a, left_value, right_value):
 a = [1, 2, 3, 3, 3, 4, 4, 8, 9]
 print(count_by_range(a, 4, 4))   # 값이 4인 데이터 개수 출력
 print(count_by_range(a, -1, 3))   # 값이 -1 ~ 3 범위에 있는 개수 출력
+
+
+
+# 부품 찾기
+def q1():
+    N = int(sys.stdin.readline().rstrip())
+    arr1 = list(map(int, sys.stdin.readline().rstrip().split()))
+    M = int(sys.stdin.readline().rstrip())
+    arr2 = list(map(int, sys.stdin.readline().rstrip().split()))
+
+    arr1.sort()
+
+    print(arr1, "\n", arr2)
+    for target in arr2:
+        left, right = 0, len(arr1) - 1
+        flag = False
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if arr1[mid] == target:
+                flag = True
+                break
+            elif arr1[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        if flag:
+            print("yes")
+        else:
+            print("no")
+
+
+# 떡볶이 떡 만들기
+def q2():
+    N, M = map(int, sys.stdin.readline().rstrip().split())
+    arr = list(map(int, sys.stdin.readline().rstrip().split()))
+    arr.sort()
+    answer = 0
+
+    left, right = 0, max(arr)
+
+    while left <= right:
+        mid = (left + right) // 2
+        hap = 0
+
+        for i in arr:
+            if i > mid:
+                hap += (i - mid)
+
+        if hap < M:
+            right = mid - 1
+        else:
+            answer = mid
+            left = mid + 1
+    
+    print(left)
+
+
+q2()
--- Algorithm/Binary_Search2.py
+++ Algorithm/Binary_Search2.py
@@ -1,3 +1,4 @@
+import sys
 from typing import *
 
 
@@ -19,4 +20,69 @@ def search(self, nums: List[int], target: int) -> int:
     return -1
 
 
-# 2. 이진 탐색 모듈
+# 정렬된 배열에서 특정 수의 개수 구하기
+def q1():
+    N, x = map(int, sys.stdin.readline().rstrip().split())
+    arr = list(map(int, sys.stdin.readline().rstrip().split()))
+
+    answer1, answer2 = -1, -1
+
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == x:
+            answer1 = mid
+            right = mid - 1
+        elif arr[mid] < x:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == x:
+            answer2 = mid
+            left = mid + 1
+        elif arr[mid] < x:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    if answer1 == -1 and answer2 == -1:
+        print(-1)
+    elif answer1 != -1 and answer1 == answer2:
+        print(1)
+    else:
+        print(answer2 - answer1 + 1)
+
+
+# 고정점 찾기
+def q2():
+    N = int(sys.stdin.readline().rstrip())
+    arr = list(map(int, sys.stdin.readline().rstrip().split()))
+
+    left, right = 0, len(arr) - 1
+    flag = False
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if mid == arr[mid]:
+            flag = True
+            break
+        elif mid < arr[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    if flag:
+        print(mid)
+    else:
+        print(-1)
+        
+
+
+q2()
